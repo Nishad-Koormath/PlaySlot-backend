8016a338916ffad47e6a7a3700ae070dd3ec624b
nishad <nishadkoormath@gmail.com>
2025-10-25 23:31:26 +0530

    add Turf model and admin configuration

---
+++ apps/turf/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Turf(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='turfs')
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null=True)
+    price_per_hour = models.DecimalField(max_digits=6, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    craeted_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+class TurfImage(models.Model):
+    turf = models.ForeignKey(Turf, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='turfs/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Image for {self.turf.name}"
+    
