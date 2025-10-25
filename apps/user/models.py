86b1dabf0d7d3cb46df406a142369e276025a017
nishad <nishadkoormath@gmail.com>
2025-10-25 18:31:08 +0530

    setup user authentication with JWT (register, login, profile)

---
+++ apps/user/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    is_turf_owner = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.username
